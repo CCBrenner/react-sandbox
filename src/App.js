b9dd06984fdf116d6d82d6d7b4d38775542e2d91
Collyn Brenner <collynbrenner@gmail.com>
2022-03-15 08:10:38 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import { Button } from "react-bootstrap";
+import './App.css';
+
+function App() {
+  return (
+    <nav>
+      <Button>Button 1</Button>
+      <Button>Button 2</Button>
+      <Button>Button 3</Button>
+      <Button>Button 4</Button>
+    </nav>
+  );
+}
+
+export default App;
